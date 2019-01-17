28c4acae46e6adc21fa2a1089339b403cdc13513
evan geiss <egeiss03@yahoo.com>
2019-01-17 15:25:21 -0500

    added test aoton

--- src/main/java/frc/robot/command/groups/evanTest.java
+++ src/main/java/frc/robot/command/groups/evanTest.java
@@ -10,8 +10,14 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class evanTest extends CommandGroup {
 
     public evanTest() {
-        addSequential(new DriveTo(12, .5, 0, 10));
+        addSequential(new DriveTo(8, .3, 0, 5));
         addSequential(new RotatePID(90));
-        addSequential(new DriveTo(12, .3, 0, 2));
+        addSequential(new DriveTo(8, .3, 90, 5));
+        addSequential(new RotatePID(180));
+        addSequential(new DriveTo(8, .3, 180, 5));
+        addSequential(new RotatePID(270));
+        addSequential(new DriveTo(8, .3, 270, 5));
+        addSequential(new RotatePID(360));
+        addSequential(new DriveTo(8, .3, 360, 5));
     }
 }
