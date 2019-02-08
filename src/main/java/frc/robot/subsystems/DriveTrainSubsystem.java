8bef06854d19a586359d7553554ca3cd46573ce5
Isaiah Kahler <isaiahwkahler@gmail.com>
2019-02-08 16:30:07 -0500

    added smartdashboard putters in drivetrainsub

--- src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -9,6 +9,8 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 //import hardware
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Ultrasonic;
@@ -31,23 +33,21 @@ public class DriveTrainSubsystem extends Subsystem {
     private Encoder leftEncoder;
     private Encoder rightEncoder;
 
-    //constants
-    private final static double wheelDiameterInches = 6.0;
+    // constants
+    private final static double wheelDiameterInches = 8.0;
     private final static double encoderClicksPerRevolution = 360;
-        //inches the ultrasonic is mounted from the front of the robot
-    private final static double ultrasonicOffset = 13; 
+    // inches the ultrasonic is mounted from the front of the robot
+    private final static double ultrasonicOffset = 13;
 
-
-
-    public DriveTrainSubsystem(SpeedController leftDrive, SpeedController rightDrive, Encoder leftEncoder, Encoder rightEncoder,
-            Ultrasonic ultrasonic, AHRS navx) {
+    public DriveTrainSubsystem(SpeedController leftDrive, SpeedController rightDrive, Encoder leftEncoder,
+            Encoder rightEncoder, Ultrasonic ultrasonic, AHRS navx) {
         this.leftMotors = leftDrive;
         this.rightMotors = rightDrive;
         this.leftEncoder = leftEncoder;
         this.rightEncoder = rightEncoder;
         this.sonic = ultrasonic;
         this.navx = navx;
-        if(this.leftMotors != null && this.rightMotors != null)
+        if (this.leftMotors != null && this.rightMotors != null)
             this.drivetrain = new DifferentialDrive(this.leftMotors, this.rightMotors);
         else
             this.drivetrain = null;
@@ -57,78 +57,87 @@ public class DriveTrainSubsystem extends Subsystem {
     public void initDefaultCommand() {
     }
 
-
-	public double getRotation() {
-        if(navx != null)
+    public double getRotation() {
+        if (navx != null)
             return navx.getAngle();
         else
             return -1;
-	}
+    }
 
-	public double getLeftEncoder() {
-        if(leftEncoder != null)
+    public double getLeftEncoder() {
+        if (leftEncoder != null)
             return (double) leftEncoder.get();
         else
             return -1;
-	}
+    }
 
-	public double getRightEncoder() {
-        if(rightEncoder != null)
+    public double getRightEncoder() {
+        if (rightEncoder != null)
             return (double) rightEncoder.get();
         else
             return -1;
-	}
+    }
 
 	/*
 	 *                  1 revolution    pi * wheel diameter inches
 	 * encoder clicks * ------------ * ---------------------------- = inches traveled
 	 *                    # clicks             1 revolution
 	 */
-	public double getLeftEncoderInches() {
-		return getLeftEncoder() * (1 / encoderClicksPerRevolution) * (Math.PI * wheelDiameterInches);
-	}
+    public double getLeftEncoderInches() {
+        return getLeftEncoder() * (1 / encoderClicksPerRevolution) * (Math.PI * wheelDiameterInches);
+    }
 
-	public double getRightEncoderInches() {
-		return getRightEncoder() * (1 / encoderClicksPerRevolution) * (Math.PI * wheelDiameterInches);
-	}
+    public double getRightEncoderInches() {
+        return getRightEncoder() * (1 / encoderClicksPerRevolution) * (Math.PI * wheelDiameterInches);
+    }
 
-	public void resetEncoders() {
-        if(leftEncoder != null)
+    public void resetEncoders() {
+        if (leftEncoder != null)
             leftEncoder.reset();
-        if(rightEncoder != null)
-		    rightEncoder.reset();
-	}
+        if (rightEncoder != null)
+            rightEncoder.reset();
+    }
 
     public void drive(double leftPower, double rightPower) {
-        if(this.drivetrain != null)
-		    drivetrain.tankDrive(curveInput(leftPower), curveInput(rightPower));
+        if (this.drivetrain != null)
+            drivetrain.tankDrive(curveInput(leftPower), curveInput(rightPower));
     }
-    
-	public void arcadeDrive(double speed, double rotation ) {
-        if(this.drivetrain != null)
-		    drivetrain.arcadeDrive(curveInput(speed), curveInput(rotation));
-	}
 
-	private double curveInput(double input) {
-		double adjustedInput = Math.pow(Math.abs(input), (1.0/3.0));
-		if(input < 0) {
-			adjustedInput *= -1;
-		}
-		return adjustedInput;
-	}
-	
-	public void resetCompass() {
-        if(navx != null)
-		    navx.reset();
-	}
+    public void arcadeDrive(double speed, double rotation) {
+        if (this.drivetrain != null)
+            drivetrain.arcadeDrive(curveInput(speed), curveInput(rotation));
+    }
 
-	public double getSonicDistance() {
-        if(sonic != null)
+    private double curveInput(double input) {
+        double adjustedInput = Math.pow(Math.abs(input), (1.0 / 3.0));
+        if (input < 0) {
+            adjustedInput *= -1;
+        }
+        return adjustedInput;
+    }
+
+    public void resetCompass() {
+        if (navx != null)
+            navx.reset();
+    }
+
+    public double getSonicDistance() {
+        if (sonic != null)
             return sonic.getRangeInches() - ultrasonicOffset;
         else
             return -1;
     }
 
-    
+    public void putEncodersOnDash() {
+        if (this.leftEncoder != null)
+            SmartDashboard.putNumber("Left Encoder Value               ", getLeftEncoder());
+        if (this.rightEncoder != null)
+            SmartDashboard.putNumber("Right Encoder Value              ", getRightEncoder());
+    }
+
+    public void putCompassOnDash() {
+        if(navx != null)
+            SmartDashboard.putNumber("Gyroscopic angle in degrees      ", getRotation());
+    }
 
 }
