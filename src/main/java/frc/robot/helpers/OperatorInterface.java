e2d0c487f29bfd735cf0a8eb9e8469f432991711
Isaiah Kahler <isaiahwkahler@gmail.com>
2019-02-14 18:08:18 -0500

    added POV function to operator interface

--- src/main/java/frc/robot/helpers/OperatorInterface.java
+++ src/main/java/frc/robot/helpers/OperatorInterface.java
@@ -6,6 +6,7 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot.helpers;
+
 import frc.robot.Robot;
 import frc.robot.OI.AxisName;
 import frc.robot.OI.ButtonName;
@@ -13,20 +14,20 @@ import frc.robot.OI.ButtonName;
 import frc.robot.helpers.ButtonFunction;
 import frc.robot.helpers.AxisFunction;
 
-public class OperatorInterface{
+public class OperatorInterface {
 
     public static void setButtonFunction(ButtonName button, boolean driverController, ButtonFunction function) {
-        if(driverController) {
-            if(Robot.m_oi.getDriverButton(button)) {
-                try{
+        if (driverController) {
+            if (Robot.m_oi.getDriverButton(button)) {
+                try {
                     function.call();
                 } catch (Exception e) {
                     System.out.println("Could not run " + function.toString());
                 }
             }
         } else {
-            if(Robot.m_oi.getOperatorButton(button)){
-                try{
+            if (Robot.m_oi.getOperatorButton(button)) {
+                try {
                     function.call();
                 } catch (Exception e) {
                     System.out.println("Could not run " + function.toString());
@@ -35,30 +36,31 @@ public class OperatorInterface{
         }
     }
 
-    public static void setButtonFunction(ButtonName button, boolean driverController, ButtonFunction truthyFunction, ButtonFunction falsyFunction) {
-        if(driverController) {
-            if(Robot.m_oi.getDriverButton(button)) {
-                try{
+    public static void setButtonFunction(ButtonName button, boolean driverController, ButtonFunction truthyFunction,
+            ButtonFunction falsyFunction) {
+        if (driverController) {
+            if (Robot.m_oi.getDriverButton(button)) {
+                try {
                     truthyFunction.call();
                 } catch (Exception e) {
                     System.out.println("Could not run " + truthyFunction.toString());
                 }
             } else {
-                try{
+                try {
                     falsyFunction.call();
                 } catch (Exception e) {
                     System.out.println("Could not run " + falsyFunction.toString());
                 }
             }
         } else {
-            if(Robot.m_oi.getOperatorButton(button)){
-                try{
+            if (Robot.m_oi.getOperatorButton(button)) {
+                try {
                     truthyFunction.call();
                 } catch (Exception e) {
                     System.out.println("Could not run " + truthyFunction.toString());
                 }
             } else {
-                try{
+                try {
                     falsyFunction.call();
                 } catch (Exception e) {
                     System.out.println("Could not run " + falsyFunction.toString());
@@ -68,11 +70,27 @@ public class OperatorInterface{
     }
 
     public static void setAxisFunction(AxisName axisName, AxisFunction function) {
-        try{
+        try {
             function.call(Robot.m_oi.getDriverAxis(axisName));
         } catch (Exception e) {
             System.out.println("Could not run " + function.toString());
         }
     }
-}
 
+    public static void setPOVFunction(boolean driverController, POVFunction function) {
+        if (driverController) {
+            try {
+                function.call(Robot.m_oi.getDriverPOV());
+            } catch (Exception e) {
+                System.out.println("Could not run " + function.toString());
+            }
+        } else {
+            try {
+                function.call(Robot.m_oi.getOperatorPOV());
+            } catch (Exception e) {
+                System.out.println("Could not run " + function.toString());
+            }
+
+        }
+    }
+}
